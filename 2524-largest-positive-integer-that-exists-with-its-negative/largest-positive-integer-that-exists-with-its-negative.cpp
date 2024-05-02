95fdabc61f5332b57b2d13f80ed9ee3650f210e9
Hariom Patel <101473914+HariomPatel2002@users.noreply.github.com>
2024-05-02 19:59:50 +0530

    Time: 15 ms (79.95%) | Memory: 27.8 MB (5.20%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        std::unordered_set<int> s(nums.begin(), nums.end()); // Store nums in a set
-        std::vector<int> arr;
+        unordered_set<int> s(nums.begin(), nums.end());
+        vector<int> arr;
 
         for (int num : nums) {
-            if (s.find(-num) != s.end()) { // Check if -num exists in the set
-                arr.push_back(num); // Store num in arr if -num exists
+            if (s.find(-num) != s.end()) { 
+                arr.push_back(num);
             }
         }
 
