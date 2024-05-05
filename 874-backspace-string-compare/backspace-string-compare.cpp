8c20562312ab96f679715af1e137fd4dac39f6dd
Hariom Patel <101473914+HariomPatel2002@users.noreply.github.com>
2024-05-05 22:45:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (37.91%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>s1;
+        stack<char>s2;
+        for(auto it:s){
+            if(!s1.empty() && it=='#'){
+                s1.pop();
+            }
+            else if(s1.empty() && it=='#'){
+                continue;
+            }
+            else{
+                s1.push(it);
+            }
+        }
+        for(auto it:t){
+            if(!s2.empty() && it=='#'){
+                s2.pop();
+            }
+            else if(s2.empty() && it=='#'){
+                continue;
+            }
+            else{
+                s2.push(it);
+            }
+        }
+        string str1,str2;
+        while(!s1.empty()){
+            char k = s1.top();
+            str1+=k;
+            s1.pop();
+        }
+        while(!s2.empty()){
+            char k = s2.top();
+            str2+=k;
+            s2.pop();
+        }
+        reverse(str1.begin(),str1.end());
+        reverse(str2.begin(),str2.end());
+        if(str1==str2) return true;
+        else return false;
+    }
+};
